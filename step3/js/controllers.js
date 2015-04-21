0515924a5bd7970286f6538986d071490b8b9c8e
tbadaczewski <badaczewski@gmail.com>
2015-04-21 10:20:00 -0400

    Starting with just step 1

--- step3/js/controllers.js
+++
@@ -1,9 +0,0 @@
-var demoApp = angular.module('demoApp', []);
-
-demoApp.controller('PhoneListCtrl', function ($scope, $http) {
-	$http.get('/json/phones.json').success(function(data) {
-		$scope.phones = data;
-	});
-
-	$scope.orderProp = 'age';
-});
